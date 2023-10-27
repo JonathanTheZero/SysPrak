9359b6a1a6b831aa1cfb5b57590b2e0d428fd369
JonathanTheZero <jonathan.frohne@web.de>
2023-10-27 16:38:24 +0200

    Zahlenraten v1

---
+++ exercises/zahlenraten/main.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main()
+{
+	srand(time(NULL));
+	int num = rand() % 100 + 1;
+	printf("Enter your guess (1 - 100):\n");
+	int guess, guesses = 1;
+	scanf("%d", &guess);
+	while (guess != num)
+	{
+		printf("Sadly that's not it! Hint: The answer is %s.\n", guess > num ? "smaller" : "bigger");
+		guesses++;
+		printf("Enter your guess:\n");
+		scanf("%d", &guess);
+	}
+	printf("Congratulations! The answer was %i and you got it in %i guesses!\n", num, guesses);
+
+	return 0;
+}
